d88b879480555933df53257cfea63f8b9bf97896
René Diana <rene.diana@netscrapers.com>
2020-12-22 16:42:59 +0100

    Finished PalindromeChecker

--- Project/coolOrange_CandidateChallenge/PalindromeChecker.cs
+++ Project/coolOrange_CandidateChallenge/PalindromeChecker.cs
@@ -13,7 +13,7 @@ namespace coolOrange_CandidateChallenge
             if (s.Length == 0 || s.Length == 1)
                 return true;
             else if (s[0] == s[s.Length - 1])
-                return IsPalindrome(s.Substring(1, s.Length - 1));
+                return IsPalindrome(s.Substring(1, s.Length - 2));
             return false;
         }
     }
